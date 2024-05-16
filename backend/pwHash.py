97dc613f99f937d5fbb47e02da7bbb62f681ddc9
RLGingerBiscuit <rlgingerbiscuit@proton.me>
2024-05-16 09:43:31 +0100

    delete everything for the rewrite

--- backend/pwHash.py
+++
@@ -1,9 +0,0 @@
-from bcrypt import hashpw, gensalt, checkpw
-
-class pwHash:
-    def __init__(self):
-        pass
-    def hash(self, password):
-        return hashpw(password.encode('utf-8'), gensalt())
-    def check(self, password, hashed):
-        return checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
